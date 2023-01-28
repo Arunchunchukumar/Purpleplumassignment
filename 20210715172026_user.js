8f6e7175b897a337faca38109005beb72337c14e
Arunchunchukumar <123556642+Arunchunchukumar@users.noreply.github.com>
2023-01-28 14:46:11 +0530

    Update 20210715172026_user.js

--- 20210715172026_user.js
+++ 20210715172026_user.js
@@ -5,6 +5,13 @@ exports.up = function(knex) {
     table.string('name',255).notNullable()
     table.string('email',255).notNullable()
     table.string('password',255).notNullable()
+    table.string('phone',255).notNullable()
+    table.date('DOB',255).notNullable()
+    table.date('createdOn').notNullable()
+    table.date('createdBy').notNullable()
+    table.date('UpdatedOn').notNullable()
+    table.date('UpdatedBy').notNullable()
+
   })
 };
 
